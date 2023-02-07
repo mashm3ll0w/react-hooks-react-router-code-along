a20ca79ffe4736cc41214eb59675abdcccd650ae
Charles Swaleh <charlswall@live.com>
2023-02-07 10:54:11 +0300

    Install react-router-dom, define the basic routes; home, about and login

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Home!</h1>
+		</div>
+	);
+}
+
+function About() {
+	return (
+		<div>
+			<h1>This is my about component!</h1>
+		</div>
+	);
+}
+
+function Login() {
+	return (
+		<div>
+			<h1>Login</h1>
+			<form>
+				<div>
+					<input type="text" name="username" placeholder="Username" />
+				</div>
+				<div>
+					<input type="password" name="password" placeholder="Password" />
+				</div>
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
+	);
 }
 
 function App() {
-  return <Home />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/about" element={<About />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
